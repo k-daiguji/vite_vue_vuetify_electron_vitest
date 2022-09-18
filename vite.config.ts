f410570ba1a3b40cefac6a6479116a72da754138
k-daiguji <hikatteru20@gmail.com>
2022-09-18 22:18:45 +0900

    リファクタリング
    リファクタリング
    
    リファクタリング2
    
    リファクタリング2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
-
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import * as path from "path";
 import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
