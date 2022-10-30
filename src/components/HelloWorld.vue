84f154f1d58fb923a63f11c1625c9ed56be56640
k-daiguji <hikatteru20@gmail.com>
2022-10-30 22:13:20 +0900

    キャンバス移動処理(縮小)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,15 +16,15 @@ const resize = (): void => {
   //   alert("output");
   // }, time * 1000);
   const img: HTMLImageElement = document.getElementById("my_canvas") as HTMLImageElement;
+  if (imgScale === 0.5) {
+    img.src = "http://placehold.jp/150x100.png"
+  }
   if (imgScale === 1) {
     img.src = "http://placehold.jp/300x200.png"
   } else if (imgScale === 2) {
     img.src = "http://placehold.jp/600x400.png"
   } else if (imgScale === 4) {
     img.src = "http://placehold.jp/1200x800.png"
-  } else {
-    img.src = "http://placehold.jp/300x200.png"
-    imgScale = 1;
   }
 };
 
@@ -41,39 +41,54 @@ const zoom = (e: WheelEvent) => {
 
   // 倍率設定
   imgScale = e.deltaY < 0 ? imgScale * 2 : imgScale / 2;
-  console.log("scale: ", imgScale);
   if (4 < imgScale) {
     imgScale = 4;
     return;
-  } else if (imgScale < 1) {
-    imgScale = 1;
+  } else if (imgScale < 0.5) {
+    imgScale = 0.5;
     return;
   }
-  img.style.padding = "0px 0px 0px 0px";
   resize();
 
+  img.style.padding = "0px 0px 0px 0px";
   const mousePointerX = e.offsetX - prevPaddingLeft;
   const mousePointerY = e.offsetY - prevPaddingTop;
-  const paddingLeft = prevPaddingLeft - mousePointerX;
-  const paddingTop = prevPaddingTop - mousePointerY;
-  const scrollLeft = paddingLeft < 0 ? -paddingLeft + prevScrollLeft : 0;
-  const scrollTop = paddingTop < 0 ? -paddingTop + prevScrollTop : 0;
+  if (e.deltaY < 0) {
+    const paddingLeft: number = prevPaddingLeft - mousePointerX;
+    const paddingTop: number = prevPaddingTop - mousePointerY;
+    const scrollLeft: number = paddingLeft < 0 ? -paddingLeft + prevScrollLeft : 0;
+    const scrollTop: number = paddingTop < 0 ? -paddingTop + prevScrollTop : 0;
 
-  img.style.paddingLeft = paddingLeft < 0 ? "0px" : `${paddingLeft}px`;
-  img.style.paddingRight = `${canvasArea.clientWidth - (paddingLeft + (img.clientWidth - scrollLeft))}px`;
-  canvasArea.scrollLeft = scrollLeft;
-  console.log("width: ", img.style.paddingLeft, img.style.paddingRight, canvasArea.scrollLeft);
-  img.style.paddingTop = paddingTop < 0 ? "0px" : `${paddingTop}px`;
-  img.style.paddingBottom = `${canvasArea.clientHeight - (paddingTop + (img.clientHeight - scrollTop))}px`;
-  canvasArea.scrollTop = scrollTop;
-  console.log("width: ", img.style.paddingTop, img.style.paddingBottom, canvasArea.scrollTop);
+    img.style.paddingLeft = paddingLeft < 0 ? "0px" : `${paddingLeft}px`;
+    img.style.paddingRight = `${canvasArea.clientWidth - (img.clientWidth - scrollLeft)}px`;
+    canvasArea.scrollLeft = scrollLeft;
+    img.style.paddingTop = paddingTop < 0 ? "0px" : `${paddingTop}px`;
+    img.style.paddingBottom = `${canvasArea.clientHeight - (img.clientHeight - scrollTop)}px`;
+    canvasArea.scrollTop = scrollTop;
+  }
+  else {
+    const width: number = (canvasArea.clientWidth - img.clientWidth) / 2;
+    const paddingLeft: number = prevPaddingLeft + (mousePointerX - prevScrollLeft) / 2;
+    const scrollLeft: number = paddingLeft < 0 ? width - paddingLeft + prevScrollLeft : 0;
+    const paddingTop: number = prevPaddingTop + (mousePointerY - prevScrollTop) / 2;
+    const scrollTop: number = paddingTop < 0 ? width - paddingTop + prevScrollTop : 0;
+
+    img.style.paddingLeft = paddingLeft < 0 ? "0px" : `${paddingLeft}px`;
+    console.log(`canvasArea.clientWidth - (img.clientWidth - scrollLeft)px`, canvasArea.clientWidth, img.clientWidth, scrollLeft)
+    console.log(`${canvasArea.clientWidth - (img.clientWidth - scrollLeft)}px`)
+    img.style.paddingRight = `${canvasArea.clientWidth - (img.clientWidth - scrollLeft)}px`;
+    canvasArea.scrollLeft = scrollLeft;
+    img.style.paddingTop = paddingTop < 0 ? "0px" : `${paddingTop}px`;
+    img.style.paddingBottom = `${canvasArea.clientWidth - (img.clientWidth - scrollTop)}px`;
+    canvasArea.scrollTop = scrollTop;
+  }
 }
 
 const currentPosition = (e: MouseEvent) => {
   const img: HTMLImageElement = document.getElementById("my_canvas") as HTMLImageElement;
   const prevPaddingLeft = Number(img.style.paddingLeft.replace('px', ''));
   const prevPaddingTop = Number(img.style.paddingTop.replace('px', ''));
-  console.log("click: ", e.offsetX - prevPaddingLeft, e.offsetY - prevPaddingTop)
+  console.log("click: ", e.offsetX - prevPaddingLeft, e.offsetY - prevPaddingTop);
 }
 </script>
 
